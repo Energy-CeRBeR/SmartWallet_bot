9f146f9f08e0de8bce10d6fc33b2c16dff333875
Igor Chesnykh (notebook) <igor.chesnyx@mail.ru>
2024-03-15 21:59:50 +0300

    Update database strucrute. All modules transfered to database.py

--- src/database.py
+++ src/database.py
@@ -1,20 +1,97 @@
-from typing import AsyncGenerator
+import datetime
 
-from sqlalchemy import MetaData
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker, AsyncAttrs
-from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import ForeignKey
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 from config_data.config import Config, load_config
 
 config: Config = load_config(".env")
 DATABASE_URL = config.database.url
-Base: DeclarativeMeta = declarative_base()
 
 engine = create_async_engine(DATABASE_URL, echo=True)
-async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+async_session = async_sessionmaker(engine)
 
 
 class Base(AsyncAttrs, DeclarativeBase):
     pass
 
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    tg_id: Mapped[int] = mapped_column(nullable=False)
+    username: Mapped[str] = mapped_column()
+    email: Mapped[str] = mapped_column()
+
+    cards = relationship("Card")
+    in_categories = relationship("IncomeCategory")
+    ex_categories = relationship("ExpenseCategory")
+
+
+class IncomeCategory(Base):
+    __tablename__ = "in_categories"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+    incomes = relationship("Income")
+
+
+class ExpenseCategory(Base):
+    __tablename__ = "ex_categories"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+    expenses = relationship("Expense")
+
+
+class CardType(Base):
+    __tablename__ = "card_types"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+
+    cards = relationship("Card")
+
+
+class Card(Base):
+    __tablename__ = "cards"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    type_id: Mapped[int] = mapped_column(ForeignKey("card_types.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    balance: Mapped[float] = mapped_column(nullable=False)
+
+
+class Income(Base):
+    __tablename__ = "incomes"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    category_id: Mapped[int] = mapped_column(ForeignKey("in_categories.id"))
+    card_id: Mapped[int] = mapped_column(ForeignKey("cards.id"))
+    amount: Mapped[float] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column()
+    date: Mapped[datetime.date] = mapped_column(nullable=False)
+
+
+class Expense(Base):
+    __tablename__ = "expenses"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    category_id: Mapped[int] = mapped_column(ForeignKey("ex_categories.id"))
+    card_id: Mapped[int] = mapped_column(ForeignKey("cards.id"))
+    amount: Mapped[float] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column()
+    date: Mapped[datetime.date] = mapped_column(nullable=False)
+
+
+async def async_main():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
---
+++ src/run.py
@@ -0,0 +1,19 @@
+import asyncio
+
+from aiogram import Dispatcher, Bot
+from config_data.config import Config, load_config
+from src.database import async_main
+
+
+async def main():
+    await async_main()
+    config: Config = load_config(".env")
+    bot = Bot(token=config.tg_bot.token, parse_mode="HTML")
+    dp = Dispatcher()
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
